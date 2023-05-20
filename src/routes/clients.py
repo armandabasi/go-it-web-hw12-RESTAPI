ce1437857ec63b1d832228615da4ba8f9f5deacb
unknown <514ftg@gmail.com>
2023-05-20 03:42:46 +0300

    first steep

---
+++ src/routes/clients.py
@@ -0,0 +1,76 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException, status, Path, Query, Depends
+from sqlalchemy.orm import Session
+
+from src.database.db import get_db
+from src.database.models import Client, User
+from src.schemas import ClientResponse, ClientModel
+from src.repository import clients as repository_clients
+from src.services.auth import auth_service
+
+router = APIRouter(prefix="/clients", tags=["Clients"])
+
+
+@router.get("/", response_model=List[ClientResponse])
+async def get_clients(limit: int = Query(10, le=300), offset: int = 0, db: Session = Depends(get_db),
+                      _: User = Depends(auth_service.get_current_user)):
+    users = await repository_clients.get_clients(limit, offset, db)
+    return users
+
+
+@router.get("/birthday/", response_model=List[ClientResponse])
+async def get_users_birthday(days: int = Query(7, le=365), db: Session = Depends(get_db),
+                             _: User = Depends(auth_service.get_current_user)):
+    users = await repository_clients.get_birthday(days, db)
+    if users is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
+    return users
+
+
+@router.get("/search/", response_model=List[ClientResponse])
+async def search_clients(data: str, db: Session = Depends(get_db), _: User = Depends(auth_service.get_current_user)):
+    clients = await repository_clients.search_clients(data, db)
+    if clients is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
+    return clients
+
+
+@router.get("/{client_id}", response_model=ClientResponse)
+async def get_user(client_id: int = Path(ge=1), db: Session = Depends(get_db),
+                   _: User = Depends(auth_service.get_current_user)):
+    client = await repository_clients.get_client(client_id, db)
+    if client is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
+    return client
+
+
+@router.post("/", response_model=ClientResponse, status_code=status.HTTP_201_CREATED)
+async def create_users(body: ClientModel, db: Session = Depends(get_db),
+                       _: User = Depends(auth_service.get_current_user)):
+    client = await repository_clients.get_client_by_email(body.email, db)
+    if client:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Client with this email already exist")
+    client = await repository_clients.get_client_by_phone(body.phone_number, db)
+    if client:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Client with this phone already exist")
+    client = await repository_clients.create_client(body, db)
+    return client
+
+
+@router.put("/{client_id}", response_model=ClientResponse)
+async def update_user(body: ClientModel, client_id: int = Path(ge=1), db: Session = Depends(get_db),
+                      _: User = Depends(auth_service.get_current_user)):
+    client = await repository_clients.update_client(body, client_id, db)
+    if client is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
+    return client
+
+
+@router.delete("/{client_id}", response_model=ClientResponse)
+async def remove_user(client_id: int = Path(ge=1), db: Session = Depends(get_db),
+                      _: User = Depends(auth_service.get_current_user)):
+    client = await repository_clients.remove_client(client_id, db)
+    if client is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Client not found")
+    return client
